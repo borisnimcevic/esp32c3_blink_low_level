5827f590858e3d9eb370794ac2aef3bcb1a12567
Boris Nimcevic <borisnimcevic@gmail.com>
2022-04-18 21:10:31 +0200

    Added assembly - working

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -7,17 +7,67 @@
 #define GPIO_BASE 0x60004000
 #define GPIO_ENABLE_REG 0x0020
 #define GPIO_OUT_REG 0x0004
-#define DELAY_MS 500
+#define DELAY_MS 100
 
 void app_main(void)
 {
     // set as output
-    (*(volatile uint32_t*)(GPIO_BASE + GPIO_ENABLE_REG) |= (0x1 << BLINK_GPIO));
+    // (*(volatile uint32_t*)(GPIO_BASE + GPIO_ENABLE_REG) |= (0x1 << BLINK_GPIO));
+
+    //clang from godbolt
+    // C --> RISC-V rv32gc clang 14.0.0
+
+    __asm__(
+          "addi    sp, sp, -16\n\t"
+          "sw      ra, 12(sp)\n\t"
+          "sw      s0, 8(sp)\n\t"
+          "addi    s0, sp, 16\n\t"
+          "lui     a1, 393220\n\t"
+          "lw      a0, 32(a1)\n\t"
+          "ori     a0, a0, 2\n\t"
+          "sw      a0, 32(a1)\n\t"
+          "li      a0, 0\n\t"
+          "lw      ra, 12(sp)\n\t"
+          "lw      s0, 8(sp)\n\t"
+          "addi    sp, sp, 16\n\t"
+      );
 
     while (1) {
-        (*(volatile uint32_t*)(GPIO_BASE + GPIO_OUT_REG) |= (0x1 << BLINK_GPIO));
-        vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
-        (*(volatile uint32_t*)(GPIO_BASE + GPIO_OUT_REG) &= ~(0x1 << BLINK_GPIO));
-        vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
+    // set
+    __asm__(
+          "addi    sp, sp, -16\n\t"
+          "sw      ra, 12(sp)\n\t"
+          "sw      s0, 8(sp)\n\t"
+          "addi    s0, sp, 16\n\t"
+          "lui     a1, 393220\n\t"
+          "lw      a0, 4(a1)\n\t"
+          "ori     a0, a0, 2\n\t"
+          "sw      a0, 4(a1)\n\t"
+          "li      a0, 0\n\t"
+          "lw      ra, 12(sp)\n\t"
+          "lw      s0, 8(sp)\n\t"
+          "addi    sp, sp, 16\n\t"
+      );
+  // (*(volatile uint32_t*)(GPIO_BASE + GPIO_OUT_REG) |= (0x1 << BLINK_GPIO));
+      vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
+
+    //clear
+    __asm__(
+        "addi    sp, sp, -16\n\t"
+        "sw      ra, 12(sp)\n\t"
+        "sw      s0, 8(sp)\n\t"
+        "addi    s0, sp, 16\n\t"
+        "lui     a1, 393220\n\t"
+        "lw      a0, 4(a1)\n\t"
+        "andi    a0, a0, -3\n\t"
+        "sw      a0, 4(a1)\n\t"
+        "li      a0, 0\n\t"
+        "lw      ra, 12(sp)\n\t"
+        "lw      s0, 8(sp)\n\t"
+        "addi    sp, sp, 16\n\t"
+      );
+
+        // (*(volatile uint32_t*)(GPIO_BASE + GPIO_OUT_REG) &= ~(0x1 << BLINK_GPIO));
+      vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
     }
 }
